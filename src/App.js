02985aa4431cb8ba914ae9a7cfc043f9b12ecf5e
diasambajon <79763077+diasambajon@users.noreply.github.com>
2021-04-19 22:12:21 -0700

    imported style components to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import Form from "./Form.js";
 import Weather from "./Weather.js";
 import Footer from "./Footer.js"; 
 
+import "./App.css";
+import "./Form.css";
+import "./Footer.css";
+import "./Weather.css";
+import "./Description.css";
+import "./Temperature.css";
+
 function App() {
   return (
     <body>
